5c9d1d45642e25b76db1a447352f18b4ad76de1e
Pubudu-Karunanayake <pubudu.karunanayake01@gmail.com>
2025-11-19 22:59:28 +0530

    Configured exception

---
+++ elms/src/main/java/lk/kn/elms/exception/ResourceAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package lk.kn.elms.exception;
+
+public class ResourceAlreadyExistsException extends RuntimeException {
+    public ResourceAlreadyExistsException(String message) {
+        super(message);
+    }
+}
