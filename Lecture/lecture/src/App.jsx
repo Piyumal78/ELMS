c1e15b200c90372605652b25287706a587280aad
Heshan Chanuka <karunar-ec21029@stu.kln.ac.lk>
2025-12-10 23:50:01 +0530

    feat: initialize React project with Vite and Tailwind CSS
    - Added package.json with dependencies for React, Tailwind CSS, and Vite.
    - Included Vite logo SVG in the public directory.
    - Created main application component (App.jsx) and a Home component.
    - Added React logo SVG in assets.
    - Set up global CSS file to import Tailwind CSS styles.
    - Configured Vite with React and Tailwind CSS plugins in vite.config.js.

---
+++ Lecture/lecture/src/App.jsx
@@ -0,0 +1,16 @@
+import Home from "./assets/page/Home"
+
+function App() {
+
+
+  return (
+    <>
+      <div className="bg-red-400 flex justify-center items-center h-screen">
+        <span className="text-4xl text-green-500 font-bold">I am heshan</span>
+        <Home />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ Lecture/lecture/src/assets/page/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="bg-red-400 flex justify-center items-center h-screen">
+        <span className="text-4xl text-green-500 font-bold">I am heshan</span>
+        </div>
+    )
+}
+
+export default Home
